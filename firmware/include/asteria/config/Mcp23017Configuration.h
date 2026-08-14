69684c6cadb3f79cf09f838124e3d04dbc981ad9
Alexandre Ba <alexandre64.ba@gmail.com>
2026-08-15 00:31:40 +0200

    feat(platform): validate MCP23017 I2C integration

--- firmware/include/asteria/config/Mcp23017Configuration.h
+++ firmware/include/asteria/config/Mcp23017Configuration.h
@@ -5,7 +5,7 @@
 namespace asteria::config::mcp23017
 {
 
-    constexpr uint8_t I2C_ADDRESS = 0x20U;
+    constexpr uint8_t I2C_ADDRESS = 0x27U;
 
     constexpr uint8_t RIGHT_ASCENSION_ENABLE_PIN = 0U; // GPA0
     constexpr uint8_t DECLINATION_ENABLE_PIN = 1U;     // GPA1
